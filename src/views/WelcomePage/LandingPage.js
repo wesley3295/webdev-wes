8744f9c13bf658e7ee3b9b3afd7ead3c9c08f3f1
Wesley <wspencer3295@yahoo.com>
2021-09-26 13:58:15 -0700

    landing page comp

---
+++ src/views/WelcomePage/LandingPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Header from "./Sections/HeaderSection";
+const LandingPage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default LandingPage;
